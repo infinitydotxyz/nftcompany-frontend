25f6b8c6657914cf0097c2e7e010649404b1df56
jsduc <ducjava@gmail.com>
2021-08-06 17:45:30 -0700

    add ethers, initEthers.

---
+++ src/utils/ethersUtil.ts
@@ -0,0 +1,17 @@
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+const ethers = require("ethers");
+// const hstABI = require("human-standard-token-abi");
+
+export async function initEthers() {
+  await window.ethereum.enable();
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const network = Number(
+    window.ethereum.send({ method: "net_version" }).result
+  );
+  console.log("Account:", await signer.getAddress());
+}
