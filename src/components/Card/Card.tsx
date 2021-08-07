6386c00bd8bb2f821e8e8917b503a8763fdefce4
jsduc <ducjava@gmail.com>
2021-08-07 16:58:16 -0700

    add Link to /preview

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,39 +1,41 @@
+import Link from 'next/link';
 import styles from './CardList.module.scss';
 import Image from "next/image";
 
 export type CardData = {
   id: string;
   title: string;
-}
+};
 
-export default function Card() {
+export default function Card({ data }: { data: CardData }) {
   return (
     <div className={styles.card}>
-      <div className={styles.cardPreview}>
-        <Image src="https://ui8-crypter-nft-html.herokuapp.com/img/content/card-pic-3.jpg" alt="Card preview" layout="fill" />
+      <Link href={`/preview?id=${data.id}`}>
+        <div className={styles.cardPreview}>
+          <Image src="https://ui8-crypter-nft-html.herokuapp.com/img/content/card-pic-3.jpg" alt="Card preview" layout="fill" />
 
-        <div className={styles.cardControls}>
-          <div className="status-green card__category">purchasing !</div>
-          <button className="card__favorite">
-            <svg className="icon icon-heart">{/* <use xlink:href="#icon-heart"></use> */}</svg>
-          </button>
-          <a className="button-small card__button js-popup-open" href="#popup-bid" data-effect="mfp-zoom-in">
-            <span>Place a bid</span>
-            <svg className="icon icon-scatter-up">{/* <use xlink:href="#icon-scatter-up"></use> */}</svg>
-          </a>
+          <div className={styles.cardControls}>
+            <div className="status-green card__category">purchasing !</div>
+            <button className="card__favorite">
+              <svg className="icon icon-heart">{/* <use xlink:href="#icon-heart"></use> */}</svg>
+            </button>
+            <a className="button-small card__button js-popup-open" href="#popup-bid" data-effect="mfp-zoom-in">
+              <span>Place a bid</span>
+              <svg className="icon icon-scatter-up">{/* <use xlink:href="#icon-scatter-up"></use> */}</svg>
+            </a>
+          </div>
+        </div>
+      </Link>
+
+      <div className="card__body">
+        <div className={styles.cardLine}>
+          <div className={styles.cardTitle}>Amazing digital art</div>
+          <div className={styles.cardPrice}>2.45 ETH</div>
+        </div>
+        <div className="card__line">
+          <div className="card__counter">3 in stock</div>
         </div>
       </div>
-      <a className="card__link" href="item.html">
-        <div className="card__body">
-          <div className={styles.cardLine}>
-            <div className={styles.cardTitle}>Amazing digital art</div>
-            <div className={styles.cardPrice}>2.45 ETH</div>
-          </div>
-          <div className="card__line">
-            <div className="card__counter">3 in stock</div>
-          </div>
-        </div>
-      </a>
     </div>
   );
 }
