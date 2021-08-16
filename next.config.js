d9988a0ca48d9a7d37d8659a5a0f9bc19a587ebf
ngduc <nducus@yahoo.com>
2021-08-16 09:08:03 -0700

    upgraded NextJS; optimized png file;
    Signed-off-by: ngduc <nducus@yahoo.com>

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@ module.exports = {
   reactStrictMode: true,
   distDir: 'build',
   images: {
-    domains: ['ui8-crypter-nft-html.herokuapp.com'],
+    domains: ['ui8-crypter-nft-html.herokuapp.com']
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*).jpg',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=180, s-maxage=180, stale-while-revalidate=180'
+          }
+        ]
+      },
+      {
+        source: '/_next/image(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=180, s-maxage=180, stale-while-revalidate=180'
+          }
+        ]
+      }
+    ];
   }
-}
+};
