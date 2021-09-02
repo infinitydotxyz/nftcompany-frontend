1a80d79a0f081aa2f57c430e8185e3f0b939b2e3
ngduc <nducus@yahoo.com>
2021-09-01 19:15:39 -0700

    List page
    Signed-off-by: ngduc <nducus@yahoo.com>

--- pages/list-nfts/index.tsx
+++ pages/list-nfts/index.tsx
@@ -2,10 +2,31 @@ import React, { useState } from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Layout from 'containers/layout';
+import { Spinner } from '@chakra-ui/spinner';
+import CardList from 'components/Card/CardList';
+import { sampleData, dummyFetch } from 'utils/apiUtil';
 import pageStyles from '../../styles/Dashboard.module.scss';
-import styles from './ListNFTs.module.scss';
+import styles from '../../styles/Dashboard.module.scss';
+
+const tabTitles = ['Owned NFTs 🎨', 'Listed NFTs 🔥'];
 
 export default function ListNFTs() {
+  const [tabIndex, setTabIndex] = useState(1);
+  const [filterShowed, setFilterShowed] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const title = React.useMemo(() => {
+    return tabTitles[tabIndex];
+  }, [tabIndex]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      await setIsFetching(true);
+      await dummyFetch();
+      await setIsFetching(false);
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <Head>
@@ -14,33 +35,46 @@ export default function ListNFTs() {
       <div className={pageStyles.dashboard}>
         <div className="container container-fluid">
           <div className="section-bar">
-            <div className="right">{/* <div className="tg-title">{title}</div> */}</div>
+            <div className="">
+              <div className="tg-title">{title}</div>
+            </div>
 
-            <div className="center">{/* Center */}</div>
+            <div className="center">
+              <ul className="links">
+                <li>
+                  <a onClick={() => setTabIndex(0)} className={tabIndex === 0 ? 'active' : ''} href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+                      <path
+                        d="M14.9303 2.5V8.4C14.9303 8.84 14.4103 9.06 14.0903 8.77L12.3403 7.16C12.1503 6.98 11.8503 6.98 11.6603 7.16L9.91031 8.76C9.59031 9.06 9.07031 8.83 9.07031 8.4V2.5C9.07031 2.22 9.29031 2 9.57031 2H14.4303C14.7103 2 14.9303 2.22 14.9303 2.5Z"
+                        fill="currentColor"
+                      ></path>
+                      <path
+                        d="M16.98 2.05891C16.69 2.01891 16.43 2.26891 16.43 2.55891V8.57891C16.43 9.33891 15.98 10.0289 15.28 10.3389C14.58 10.6389 13.77 10.5089 13.21 9.98891L12.34 9.18891C12.15 9.00891 11.86 9.00891 11.66 9.18891L10.79 9.98891C10.43 10.3289 9.96 10.4989 9.49 10.4989C9.23 10.4989 8.97 10.4489 8.72 10.3389C8.02 10.0289 7.57 9.33891 7.57 8.57891V2.55891C7.57 2.26891 7.31 2.01891 7.02 2.05891C4.22 2.40891 3 4.29891 3 6.99891V16.9989C3 19.9989 4.5 21.9989 8 21.9989H16C19.5 21.9989 21 19.9989 21 16.9989V6.99891C21 4.29891 19.78 2.40891 16.98 2.05891ZM17.5 18.7489H9C8.59 18.7489 8.25 18.4089 8.25 17.9989C8.25 17.5889 8.59 17.2489 9 17.2489H17.5C17.91 17.2489 18.25 17.5889 18.25 17.9989C18.25 18.4089 17.91 18.7489 17.5 18.7489ZM17.5 14.7489H13.25C12.84 14.7489 12.5 14.4089 12.5 13.9989C12.5 13.5889 12.84 13.2489 13.25 13.2489H17.5C17.91 13.2489 18.25 13.5889 18.25 13.9989C18.25 14.4089 17.91 14.7489 17.5 14.7489Z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                    {tabTitles[0].slice(0, -2)}
+                  </a>
+                </li>
+                <li>
+                  <a onClick={() => setTabIndex(1)} className={tabIndex === 1 ? 'active' : ''} href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                      <path
+                        d="M16.44 3.10156C14.63 3.10156 13.01 3.98156 12 5.33156C10.99 3.98156 9.37 3.10156 7.56 3.10156C4.49 3.10156 2 5.60156 2 8.69156C2 9.88156 2.19 10.9816 2.52 12.0016C4.1 17.0016 8.97 19.9916 11.38 20.8116C11.72 20.9316 12.28 20.9316 12.62 20.8116C15.03 19.9916 19.9 17.0016 21.48 12.0016C21.81 10.9816 22 9.88156 22 8.69156C22 5.60156 19.51 3.10156 16.44 3.10156Z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                    {tabTitles[1].slice(0, -2)}
+                  </a>
+                </li>
+              </ul>
+            </div>
 
             <div className="left">{/* TBD */}</div>
           </div>
 
           <div className={styles.main}>
-            <section className="container container-fluid grid">
-              <div className="col-md-6 col-sm-12">
-                <h3 className="tg-title">Owned NFTs</h3>
-
-                <ul style={{ marginTop: 10 }}>
-                  <li>NFT 1</li>
-                  <li>NFT 2</li>
-                </ul>
-              </div>
-
-              <div className="col-md-6 col-sm-12">
-                <h3 className="tg-title">Listed NFTs:</h3>
-
-                <ul style={{ marginTop: 10 }}>
-                  <li>NFT 1</li>
-                  <li>NFT 2</li>
-                </ul>
-              </div>
-            </section>
+            {isFetching ? <Spinner size="md" color="gray.800" /> : <CardList data={sampleData} />}
           </div>
         </div>
       </div>
