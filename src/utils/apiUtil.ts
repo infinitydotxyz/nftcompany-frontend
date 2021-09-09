27577d90c2c4bc944885baa53ee241bf21a0f8d6
Adi Kancherla <a@mavrik.co>
2021-09-08 22:38:23 -0700

    auth (#18)
    * auth
    
    * Update explore page (#17)
    
    * Update explore page
    
    * Code review
    
    * Add basePriceInEth attribute
    
    * Update NFT inteface name
    
    * auth
    
    * comments taken care of
    
    Co-authored-by: TylerFischer66 <47264648+TylerFischer66@users.noreply.github.com>

--- src/utils/apiUtil.ts
+++ src/utils/apiUtil.ts
@@ -1,6 +1,11 @@
 import axios, { AxiosInstance } from 'axios';
 import qs from 'query-string';
 import { API_BASE } from './constants';
+import {
+  getAccount,
+  getWeb3
+} from './ethersUtil'
+const personalSignAsync = require('../../src-os/src/utils/utils').personalSignAsync;
 
 const axiosApi: AxiosInstance = axios.create({
   headers: {
@@ -128,6 +133,25 @@ function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+export async function getAuthHeaders() {
+  // fetch auth signature and message from local storage
+  const localStorage = window.localStorage
+  let sig = localStorage.getItem('X-AUTH-SIGNATURE') || ''
+  let msg = localStorage.getItem('X-AUTH-MESSAGE') || 'LOGIN'
+  // if they are empty, resign and store
+  if (!sig) {
+    console.log('No auth found, re logging in')
+    const sign = await personalSignAsync(getWeb3(), msg, await getAccount())
+    sig = JSON.stringify(sign)
+    localStorage.setItem('X-AUTH-SIGNATURE', sig)
+    localStorage.setItem('X-AUTH-MESSAGE', msg)
+  }
+  return {
+    'X-AUTH-SIGNATURE': sig,
+    'X-AUTH-MESSAGE': msg
+  }
+}
+
 export async function dummyFetch() {
   await sleep(1000);
   return sampleData;
@@ -137,7 +161,7 @@ export const apiGet = async (path: string, query?: any) => {
   let queryStr = '';
   queryStr = query ? '?' + qs.stringify(query) : '';
   try {
-    const { data } = await axiosApi({ url: `${API_BASE}${path}${queryStr}`, method: 'GET' });
+    const { data } = await axiosApi({ url: `${API_BASE}${path}${queryStr}`, method: 'GET', headers: await getAuthHeaders() });
     return { result: data };
   } catch (error) {
     return { error };
@@ -148,7 +172,7 @@ export const apiPost = async (path: string, query?: any, payload?: any) => {
   let queryStr = '';
   queryStr = query ? '?' + qs.stringify(query) : '';
   try {
-    const { data } = await axiosApi({ url: `${API_BASE}${path}${queryStr}`, method: 'POST', data: payload });
+    const { data } = await axiosApi({ url: `${API_BASE}${path}${queryStr}`, method: 'POST', headers: await getAuthHeaders(), data: payload });
     return { result: data };
   } catch (error) {
     return { error };
@@ -159,7 +183,7 @@ export const apiDelete = async (path: string, query?: any) => {
   let queryStr = '';
   queryStr = query ? '?' + qs.stringify(query) : '';
   try {
-    const { data } = await axiosApi({ url: `${API_BASE}${path}${queryStr}`, method: 'DELETE' });
+    const { data } = await axiosApi({ url: `${API_BASE}${path}${queryStr}`, method: 'DELETE', headers: await getAuthHeaders() });
     return { result: data };
   } catch (error) {
     return { error };
