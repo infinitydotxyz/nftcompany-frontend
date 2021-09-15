5f5c071ecafb5d5ac9b3db0b0630e85eb5d96cfa
Tyler Fischer <tylerf66@gmail.com>
2021-09-15 14:08:16 -0500

    removed bootstrap, cherry picked css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import '../styles/grid.css';
 import '../styles/fonts.css';
+import '../styles/Typeahead.css';
 import '../src/components/nft/components.scss';
 import { AppChakraProvider } from 'utils/themeUtil';
 import type { AppProps } from 'next/app';
