db003640c6372c6d607bdb5128b243ac20cdf852
ngduc <nducus@yahoo.com>
2021-09-17 15:37:03 -0700

    reload data after cancelling a listing

--- pages/listed-nfts/index.tsx
+++ pages/listed-nfts/index.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next';
 import Head from 'next/head';
 import Layout from 'containers/layout';
 import CardList from 'components/Card/CardList';
-import { apiGet } from 'utils/apiUtil';
+import { apiDelete, apiGet } from 'utils/apiUtil';
 import DeleteListingModal from './DeleteListingModal';
 import { ITEMS_PER_PAGE } from 'utils/constants';
 import { FetchMore, getLastItemCreatedAt } from 'components/FetchMore/FetchMore';
@@ -12,6 +12,7 @@ import { ordersToCardData } from 'services/Listings.service';
 import pageStyles from '../../styles/Dashboard.module.scss';
 import styles from '../../styles/Dashboard.module.scss';
 import LoadingCardList from 'components/LoadingCardList/LoadingCardList';
+import { CardData } from 'components/Card/Card';
 
 export default function ListNFTs() {
   const { user, showAppError, showAppMessage } = useAppContext();
@@ -20,19 +21,23 @@ export default function ListNFTs() {
   const [filterShowed, setFilterShowed] = useState(false);
   const [isFetching, setIsFetching] = useState(false);
   const [data, setData] = useState<any>([]);
-  const [deleteModalItem, setDeleteModalItem] = useState(null);
+  const [deleteModalItem, setDeleteModalItem] = useState<CardData | null>(null);
 
-  const fetchData = async () => {
+  const fetchData = async (isRefreshing: boolean = false) => {
     if (!user?.account) {
       setData([]);
       return;
     }
     setIsFetching(true);
     let listingData = [];
-    const newCurrentPage = currentPage + 1;
+    let newCurrentPage = currentPage + 1;
+    if (isRefreshing) {
+      newCurrentPage = 0;
+      setDataLoaded(false);
+    }
     try {
       const { result, error } = await apiGet(`/u/${user?.account}/listings`, {
-        startAfter: getLastItemCreatedAt(data),
+        startAfter: isRefreshing ? '' : getLastItemCreatedAt(data),
         limit: ITEMS_PER_PAGE
       });
       if (error) {
@@ -48,7 +53,7 @@ export default function ListNFTs() {
 
     console.log('moreData', moreData);
     setIsFetching(false);
-    setData([ ...data, ...moreData ]);
+    setData(isRefreshing ? moreData : [...data, ...moreData]);
     setCurrentPage(newCurrentPage);
   };
 
@@ -82,17 +87,15 @@ export default function ListNFTs() {
           </div>
 
           <div className={styles.main}>
-            {data?.length === 0 && isFetching && (
-              <LoadingCardList />
-            )}
+            {data?.length === 0 && isFetching && <LoadingCardList />}
             <CardList
-                data={data}
-                actions={['CANCEL_LISTING']}
-                onClickAction={async (item, action) => {
-                  console.log('item, action', item, action);
-                  setDeleteModalItem(item);
-                }}
-              />
+              data={data}
+              actions={['CANCEL_LISTING']}
+              onClickAction={async (item, action) => {
+                console.log('item, action', item, action);
+                setDeleteModalItem(item);
+              }}
+            />
           </div>
 
           {dataLoaded && (
@@ -113,10 +116,17 @@ export default function ListNFTs() {
           user={user}
           data={deleteModalItem}
           onClose={() => setDeleteModalItem(null)}
-          onSubmit={() => {
+          onSubmit={async () => {
             setDeleteModalItem(null);
-            fetchData();
-            showAppMessage(`Listing cancelled successfully.`);
+            const { error } = await apiDelete(`/u/${user?.account}/listings/${deleteModalItem?.id}`);
+            if (error) {
+              showAppError(`Failed to cancel this listing. ${error?.message}`);
+            } else {
+              showAppMessage(`Listing cancelled successfully.`);
+              setTimeout(() => {
+                fetchData(true);
+              }, 1000); // TODO: without this delay, for some reason, api still returns old data.
+            }
           }}
         />
       )}
