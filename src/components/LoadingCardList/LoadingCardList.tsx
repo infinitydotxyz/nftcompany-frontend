9aea86de673d8696567d790f247e1480e5995fd3
ngduc <nducus@yahoo.com>
2021-09-17 13:26:27 -0700

    rebased
    Signed-off-by: ngduc <nducus@yahoo.com>

---
+++ src/components/LoadingCardList/LoadingCardList.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const LoadingCard = (props: any) => (
+  <ContentLoader
+    speed={2}
+    width={250}
+    height={200}
+    viewBox="0 0 250 200"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#e3e3e3"
+    {...props}
+  >
+    <rect x="16" y="175" rx="2" ry="2" width="140" height="10" />
+    <rect x="16" y="190" rx="2" ry="2" width="140" height="10" />
+    <rect x="13" y="0" rx="2" ry="2" width="250" height="170" />
+    <rect x="200" y="175" rx="2" ry="2" width="140" height="10" />
+  </ContentLoader>
+);
+
+const LoadingCardList = () => {
+  return (
+    <div style={{ marginTop: 20 }}>
+      <LoadingCard />
+
+      <LoadingCard style={{ marginLeft: 20 }} />
+    </div>
+  );
+};
+
+export default LoadingCardList;
