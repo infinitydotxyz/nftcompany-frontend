ce5d573d2749be9cd158df7ae6beb153a6615267
Steve Gehrman <sgehrman@gmail.com>
2021-09-19 15:41:31 -0700

    subscribe button

--- src/components/SettingsModal/SettingsModal.tsx
+++ src/components/SettingsModal/SettingsModal.tsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 import styles from './SettingsModal.module.scss';
 import { useAppContext } from 'utils/context/AppContext';
 import { apiGet, apiPost } from 'utils/apiUtil';
-import { Button, Input } from '@chakra-ui/react';
+import { Button, Input, Link } from '@chakra-ui/react';
 const Modal = dynamic(() => import('hooks/useModal'));
 const isServer = typeof window === 'undefined';
 
@@ -13,12 +13,14 @@ interface Props {
 
 const SettingsModal: React.FC<Props> = ({ onClose }: Props) => {
   const [email, setEmail] = useState('');
+  const [showSubscribeSection, setShowSubscribeSection] = useState(false);
+  const [subscribed, setSubscribed] = useState(false);
 
   const { user, showAppError } = useAppContext();
 
   const updateEmail = async (email: string) => {
     if (user != null && email?.length > 0) {
-      const response = await apiPost(`/u/${user?.account}/setEmail`, null, { user: user!.account, email: email });
+      const response = await apiPost(`/u/${user?.account}/setEmail`, null, { email: email });
 
       if (response.status == 200) {
         onClose();
@@ -38,12 +40,39 @@ const SettingsModal: React.FC<Props> = ({ onClose }: Props) => {
       return;
     }
 
-    setEmail(result?.address || '');
+    // we can't check email below since setEmail is async
+    const emailFromServer = result?.address ?? '';
+
+    setEmail(emailFromServer);
+    setSubscribed(result?.subscribed ?? false);
+
+    // we don't show subscribe/unsubscribe if they don't have an email set on the server
+    setShowSubscribeSection(emailFromServer?.length > 0);
   };
 
   React.useEffect(() => {
     getEmail();
   }, [user]);
+
+  const toggleSubscribe = async () => {
+    const response = await apiPost(`/u/${user?.account}/subscribeEmail`, null, { subscribe: !subscribed });
+
+    if (response.status == 200) {
+      // refresh
+      getEmail();
+    }
+  };
+
+  const _subscribeSection = showSubscribeSection ? (
+    <div className={styles.subscribe}>
+      <div>
+        <Link color="blue.500" onClick={toggleSubscribe}>
+          {subscribed ? 'Unsubscribe' : 'Subscribe'}
+        </Link>
+        {` if you ${subscribed ? 'do not' : ''} want to receive emails about your account activity.`}
+      </div>
+    </div>
+  ) : null;
 
   return (
     <>
@@ -72,6 +101,8 @@ const SettingsModal: React.FC<Props> = ({ onClose }: Props) => {
                   }}
                   onChange={(e: any) => setEmail(e.target.value)}
                 />
+
+                {_subscribeSection}
 
                 <div className={styles.buttons}>
                   <Button colorScheme="gray" onClick={onClose}>
