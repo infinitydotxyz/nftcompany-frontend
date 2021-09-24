1f0f92830e709dd5809ac321cf6968432ab662e7
Adi Kancherla <findadit@gmail.com>
2021-09-24 16:52:08 -0700

    release cut

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,14 +1,9 @@
-// todo: adi check these before push
-
-// export const API_BASE = 'https://server.nftcompany.com';
-// export const RINKEBY_API_BASE = 'https://server.nftcompany.com';
-export const API_BASE = 'http://localhost:9090';
-export const RINKEBY_API_BASE = 'http://localhost:9090';
+export const API_BASE = 'https://server.nftcompany.com';
+export const RINKEBY_API_BASE = 'https://server.nftcompany.com';
 export const SITE_HOST = 'https://nftcompany.com';
 export const RINKEBY_SITE_HOST = 'https://nftcompany.com';
 export const PROVIDER_URL_ETH_MAINNET = 'https://eth-mainnet.alchemyapi.io/v2/umiR3YUjFlvlrDNOQQltWppf2M-p07Q3';
 export const NFTC_FEE_RECIPIENT = '0xAAdd54c429a6eEBD4514135EaD53d98D0Cc57d57';
-// export const NFTC_FEE_RECIPIENT = '0x5b3256965e7c3cf26e11fcaf296dfc8807c01073';
 
 export const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 
