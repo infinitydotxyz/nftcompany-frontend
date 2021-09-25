4cd0ee1730e82dedfbc1cfc9fcf102202a00af35
Adi Kancherla <findadit@gmail.com>
2021-09-24 19:08:16 -0700

    minor

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,4 +7,4 @@ export const NFTC_FEE_RECIPIENT = '0xAAdd54c429a6eEBD4514135EaD53d98D0Cc57d57';
 
 export const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 
-export const ITEMS_PER_PAGE = 50; // for pagination.
+export const ITEMS_PER_PAGE = 50;
