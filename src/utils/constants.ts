9d422def024316f0deba62fc08cfe90c9254af44
Adi Kancherla <findadit@gmail.com>
2021-09-24 19:54:37 -0700

    minor

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const API_BASE = 'http://localhost:9090';
 export const RINKEBY_API_BASE = 'http://localhost:9090';
-export const SITE_HOST = 'https://nftcompany.com';
-export const RINKEBY_SITE_HOST = 'https://nftcompany.com';
+export const SITE_HOST = 'http://localhost:3000';
+export const RINKEBY_SITE_HOST = 'http://localhost:3000';
 export const PROVIDER_URL_ETH_MAINNET = 'https://eth-mainnet.alchemyapi.io/v2/umiR3YUjFlvlrDNOQQltWppf2M-p07Q3';
 export const NFTC_FEE_RECIPIENT = '0xAAdd54c429a6eEBD4514135EaD53d98D0Cc57d57';
 
