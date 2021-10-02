0d4bba0cc106354c9c0ff928f0540b0ff613b410
Adi Kancherla <findadit@gmail.com>
2021-10-02 09:25:00 -0700

    constants

--- lib/ga/gtag.ts
+++ lib/ga/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || '';
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID_INFINITY || '';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
-export const API_BASE = 'https://server.nftcompany.com';
-export const RINKEBY_API_BASE = 'https://server.nftcompany.com';
-export const SITE_HOST = 'https://nftcompany.com';
-export const RINKEBY_SITE_HOST = 'https://nftcompany.com';
+export const API_BASE = 'https://sv.infinity.xyz';
+export const RINKEBY_API_BASE = 'https://sv.infinity.xyz';
+export const SITE_HOST = 'https://infinity.xyz';
+export const RINKEBY_SITE_HOST = 'https://infinity.xyz';
 export const PROVIDER_URL_ETH_MAINNET = 'https://eth-mainnet.alchemyapi.io/v2/umiR3YUjFlvlrDNOQQltWppf2M-p07Q3';
 export const NFTC_FEE_RECIPIENT = '0xAAdd54c429a6eEBD4514135EaD53d98D0Cc57d57';
 
