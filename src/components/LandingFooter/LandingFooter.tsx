8a7bab229c58e44fb944a5367593e94628d6ee97
Steve Gehrman <sgehrman@gmail.com>
2021-10-03 03:18:44 -0700

    footer

--- src/components/LandingFooter/LandingFooter.tsx
+++ src/components/LandingFooter/LandingFooter.tsx
@@ -5,20 +5,19 @@ import styles from './LandingFooter.module.scss';
 
 const LandingFooter = () => {
   return (
-    <header className={styles.footer}>
-      <div className="container container-avg">
-        <div className="grid">
-          <Box className="col-sm-12 col-md-6" p={6}>
-            <Link href="/">
-              <Image alt="Infinity" src="/img/nftcompanyTransparentBgSvg.svg" width={180} height={60} />
-            </Link>
+    <div className={styles.footer}>
+      <div className="grid">
+        <Box className="col-sm-12 col-md-6">
+          <Link href="/">
+            <Image alt="Infinity" src="/img/nftcompanyTransparentBgSvg.svg" width={180} height={60} />
+          </Link>
 
-            <p className={styles.paragraph}>
-              Infinity is a product of Mavrik Labs Inc. Mavrik labs has a grand vision of making crypto a part of a
-              billion people’s daily lives.
-            </p>
-          </Box>
-          {/* <div className="col-sm-12 col-md-3">
+          <p className={styles.paragraph}>
+            Infinity is a product of Mavrik Labs Inc. Mavrik labs has a grand vision of making crypto a part of a
+            billion people’s daily lives.
+          </p>
+        </Box>
+        {/* <div className="col-sm-12 col-md-3">
             <div className="tg-desc">Infinity</div>
             <ul className= {styles.links}>
             <li>
@@ -32,7 +31,7 @@ const LandingFooter = () => {
             </li>
             </ul>
           </div> */}
-          {/* <div className="col-sm-12 col-md-3">
+        {/* <div className="col-sm-12 col-md-3">
             <div className="tg-desc">Legal</div>
             <ul className={styles.links}>
             <li>
@@ -46,31 +45,30 @@ const LandingFooter = () => {
             </li>
             </ul>
           </div> */}
-        </div>
+      </div>
 
-        <div className={'grid ' + styles.second}>
-          <div className="col-sm-12 col-md-7">
-            <p>
-              Copyright © 2021 Mavrik labs Inc, All rights reserved | <a href="https://mavrik.co">mavrik.co</a>
-            </p>
-          </div>
-          <div className={'col-sm-12 col-md-5 justify-self-end ' + styles.socials}>
-            <a href="https://medium.com/@mavriklabs">
-              <Image alt="Infinity" src="/img/medium.svg" width={30} height={30} />
-            </a>
-            <a href="https://discord.gg/SefzVZU72S">
-              <Image alt="Infinity" src="/img/discord.svg" width={30} height={30} />
-            </a>
-            <a href="https://twitter.com/mavriklabs">
-              <Image alt="Infinity" src="/img/twitter.svg" width={30} height={30} />
-            </a>
-            <a href="https://www.instagram.com/mavriklabs/">
-              <Image alt="Infinity" src="/img/instagram.svg" width={30} height={30} />
-            </a>
-          </div>
+      <div className={'grid ' + styles.second}>
+        <div className="col-sm-12 col-md-7">
+          <p>
+            Copyright © 2021 Mavrik labs Inc, All rights reserved | <a href="https://mavrik.co">mavrik.co</a>
+          </p>
+        </div>
+        <div className={'col-sm-12 col-md-5 justify-self-end ' + styles.socials}>
+          <a href="https://medium.com/@mavriklabs">
+            <Image alt="Infinity" src="/img/medium.svg" width={30} height={30} />
+          </a>
+          <a href="https://discord.gg/SefzVZU72S">
+            <Image alt="Infinity" src="/img/discord.svg" width={30} height={30} />
+          </a>
+          <a href="https://twitter.com/mavriklabs">
+            <Image alt="Infinity" src="/img/twitter.svg" width={30} height={30} />
+          </a>
+          <a href="https://www.instagram.com/mavriklabs/">
+            <Image alt="Infinity" src="/img/instagram.svg" width={30} height={30} />
+          </a>
         </div>
       </div>
-    </header>
+    </div>
   );
 };
 
