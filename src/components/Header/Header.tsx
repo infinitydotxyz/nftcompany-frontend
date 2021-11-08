783c26971b528d671ba883f13f69eae008339ae1
Steve Gehrman <sgehrman@gmail.com>
2021-11-07 16:30:15 -0800

    new logos

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -229,20 +229,14 @@ const Header = (): JSX.Element => {
                 style={{ flex: '0 1 auto', minHeight: 68 }}
                 className="can-click"
                 alt="logo"
-                src={dark ? '/img/nftcompanyDarkModeLogo.svg' : '/img/infinity.svg'}
-                width={130}
+                src={dark ? '/img/logo-dark-mode.svg' : '/img/logo-light-mode.svg'}
+                width={180}
               />
             </Link>
           </div>
           <div className={styles.showSmallLogo}>
             <Link href="/" passHref>
-              <img
-                style={{ flex: '0 1 auto' }}
-                className="can-click"
-                alt="logo"
-                src={dark ? '/img/ncDarkMode.svg' : '/img/infinitySmall.svg'}
-                width={13}
-              />
+              <img style={{ flex: '0 1 auto' }} className="can-click" alt="logo" src="/img/logo-mini.svg" width={60} />
             </Link>
           </div>
 
