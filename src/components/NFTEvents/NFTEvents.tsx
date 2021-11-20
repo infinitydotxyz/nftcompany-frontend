62d2e98bb89228b51f270b85f7d0c69ed7902f7f
Duc Ng <nducus@yahoo.com>
2021-11-20 10:32:29 -0800

    NFTEvents component (#73)

---
+++ src/components/NFTEvents/NFTEvents.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { useColorMode, Tabs, TabPanels, TabPanel, TabList, Tab } from '@chakra-ui/react';
+import { PurchaseAccordionItem, SingleAccordion } from 'components/PurchaseAccordion/SingleAccordion';
+import { LargeIcons } from 'components/Icons/MenuIcons';
+import CollectionEvents from 'components/CollectionEvents/CollectionEvents';
+
+import styles from './NFTEvents.module.scss';
+
+interface NFTEventsProps {
+  address: string;
+  tokenId: string;
+}
+
+function NFTEvents({ address, tokenId }: NFTEventsProps) {
+  const [tabIndex, setTabIndex] = useState(0);
+  const { colorMode } = useColorMode();
+  const dark = colorMode === 'dark';
+
+  return (
+    <div className={styles.infoBox}>
+      <SingleAccordion>
+        <PurchaseAccordionItem dark={dark} title="NFT Activity" icon={LargeIcons.sortIcon}>
+          <Tabs onChange={(index) => setTabIndex(index)}>
+            <TabList className={styles.tabList}>
+              <Tab>Sales</Tab>
+              <Tab>Transfers</Tab>
+              <Tab>Offers</Tab>
+            </TabList>
+
+            <TabPanels>
+              <TabPanel>
+                {tabIndex === 0 && <CollectionEvents address={address} tokenId={tokenId} eventType="successful" />}
+              </TabPanel>
+              <TabPanel>
+                {tabIndex === 1 && <CollectionEvents address={address} tokenId={tokenId} eventType="transfer" />}
+              </TabPanel>
+              <TabPanel>
+                {tabIndex === 2 && <CollectionEvents address={address} tokenId={tokenId} eventType="bid_entered" />}
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </PurchaseAccordionItem>
+      </SingleAccordion>
+    </div>
+  );
+}
+
+export default NFTEvents;
