0899b3f19a149246972b43d651d371de9735b7b0
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-01-10 11:13:00 -0600

    Collection page (#90)
    * fixed userReady not getting set when the user isn't logged in
    
    * fixed styling issues, added eth logo, made twitter feed taller
    
    * fixed site not opening without metamask

--- src/utils/ethersUtil.ts
+++ src/utils/ethersUtil.ts
@@ -32,7 +32,6 @@ export const getProvider = () => {
     case WalletType.WalletConnect:
       return ((window.ethereum as any)?.providers ?? [window.ethereum]).find((p: any) => p.isWalletConnect);
     default:
-      return ((window.ethereum as any)?.providers ?? [window.ethereum]).find((p: any) => p.isMetaMask);
   }
 };
 
