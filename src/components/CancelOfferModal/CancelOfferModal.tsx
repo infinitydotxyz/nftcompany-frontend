4ad2d7bb3ed4426a5c4df570df52f2ae69cedd25
ngduc <nducus@yahoo.com>
2022-02-08 01:17:48 +0000

    close buttons

--- src/components/CancelOfferModal/CancelOfferModal.tsx
+++ src/components/CancelOfferModal/CancelOfferModal.tsx
@@ -85,7 +85,7 @@ const CancelOfferModal: React.FC<IProps> = ({ onClose, data }: IProps) => {
                 Cancel Offer
               </Button>
 
-              <Button disabled={isSubmitting} onClick={() => onClose && onClose()}>
+              <Button variant="outline" disabled={isSubmitting} onClick={() => onClose && onClose()}>
                 Close
               </Button>
             </div>
