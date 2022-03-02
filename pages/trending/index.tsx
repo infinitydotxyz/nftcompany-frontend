fe7755624eabb28ccff520f6649285af58455b3d
Steve Gehrman <sgehrman@gmail.com>
2022-03-02 03:50:12 -0800

    fix

--- pages/trending/index.tsx
+++ pages/trending/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Layout from 'containers/layout';
@@ -7,9 +7,13 @@ import { useAppContext } from 'utils/context/AppContext';
 import { TrendingList } from 'components/TrendingList';
 import { PleaseConnectWallet } from 'components/FetchMore/FetchMore';
 import { PageHeader } from 'components/PageHeader';
+import ToggleTab, { useToggleTab } from 'components/ToggleTab/ToggleTab';
 
 const TrendingPage = (): JSX.Element => {
   const { user, showAppError } = useAppContext();
+  const { options, onChange, selected } = useToggleTab(['Trending', 'Watchlist'], 'Trending');
+
+  const tabControl = <ToggleTab options={options} selected={selected} onChange={onChange} size="sm" />;
 
   return (
     <>
@@ -18,10 +22,10 @@ const TrendingPage = (): JSX.Element => {
       </Head>
       <div className={styles.main}>
         <div className="page-container">
-          <PageHeader title="Trending" />
+          <PageHeader title="Trending" rightSide={tabControl} />
           <PleaseConnectWallet account={user?.account} />
 
-          <TrendingList />
+          <TrendingList watchlistMode={selected === 'Watchlist'} />
         </div>
       </div>
     </>
