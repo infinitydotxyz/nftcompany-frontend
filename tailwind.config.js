5194c0d9ac6755ef78463cb3574d3396e2b37a98
John Carter <50986923+johnkcr@users.noreply.github.com>
2022-03-22 08:22:27 -0600

    feat(config): Add tailwind core color (#122)
    * configure tailwindcss fonts
    
    * remove old fonts
    
    * fix Header style
    
    * feat: configure tailwind core colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
     fontFamily: {
       heading: ['F37ZagmaMono', '-apple-system', 'sans-serif'],
       body: ['F37Bolton', '-apple-system', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        gray: {
+          dark: '#666',
+          light: '#f0f0f0'
+        }
+      }
     }
   },
   plugins: []
