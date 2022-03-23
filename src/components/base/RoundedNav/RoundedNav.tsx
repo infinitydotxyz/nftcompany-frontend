883022123282437d2db3d5097bf87a128d4e761d
ngduc <nducus@yahoo.com>
2022-03-22 21:34:18 -0700

    RoundedNav component

---
+++ src/components/base/RoundedNav/RoundedNav.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+
+interface NavItem {
+  title: string;
+  onClick?: () => void;
+}
+
+interface Props {
+  items: NavItem[];
+  defaultIndex?: number;
+  onChange?: (currentIndex: number) => void;
+}
+
+export default function RoundedNav({ items, defaultIndex, onChange }: Props) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    setActiveIndex(defaultIndex ?? 0);
+  }, [defaultIndex]);
+
+  return (
+    <nav className="flex justify-between space-x-4 bg-gray-200 rounded-2xl" aria-label="Tabs">
+      {items.map((item, index: number) => {
+        const isActive = index === activeIndex;
+        const activeCx = isActive ? 'bg-gray-900 text-white' : '';
+        return (
+          <a
+            key={item.title}
+            href="#"
+            className={`px-10 py-1 font-medium rounded-3xl text-lg bg-gray-200 ${activeCx}`}
+            onClick={() => {
+              setActiveIndex(index);
+              if (onChange) {
+                onChange(index);
+              }
+            }}
+          >
+            {item.title}
+          </a>
+        );
+      })}
+    </nav>
+  );
+}
