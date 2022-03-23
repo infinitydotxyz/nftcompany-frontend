723103d57d7fe96ace066fc9e2df4c92cee4324b
ngduc <nducus@yahoo.com>
2022-03-23 12:19:31 -0700

    minor

--- pages/sandbox/index.tsx
+++ pages/sandbox/index.tsx
@@ -5,7 +5,7 @@ import { FaFacebook, FaTwitter } from 'react-icons/fa';
 export default function SandBox() {
   return (
     <div className="flex-col space-y-8 prose p-4">
-      <h3>Text</h3>
+      <h3># Text</h3>
       <div>
         <h1 className="page-header">Page Header</h1>
         <h1>H1</h1>
@@ -17,13 +17,13 @@ export default function SandBox() {
         <div className="text-secondary">Secondary Text</div>
       </div>
 
-      <h3>Button</h3>
+      <h3># Button</h3>
       <div className="flex space-x-4">
         <button className="btn">Button 1</button>
         <button className="btn">Button 2</button>
       </div>
 
-      <h3>Chip</h3>
+      <h3># Chip</h3>
       <div className="flex">
         <Chip content="Watch" />
         <Chip content="Edit" />
@@ -31,7 +31,7 @@ export default function SandBox() {
         <Chip content={<FaFacebook />} />
       </div>
 
-      <h3>RoundedNav</h3>
+      <h3># RoundedNav</h3>
       <div className="w-1/6">
         <RoundedNav
           items={[{ title: 'NFT' }, { title: 'Community' }]}
